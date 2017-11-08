c23b861dd595be8178d1461b2dfaba1875883166
Benjamin Aaron Degenhart <b.degenhart.study@gmail.com>
2017-11-08 21:01:08 +0100

    handle communication up to joining a game

--- go_server_app/consumers.py
+++ go_server_app/consumers.py
@@ -1,16 +1,67 @@
+import json
+
 from channels.sessions import channel_session
+
+from .models import GameStatus, GameMeta
+from .utils import to_text_dict, normalize_color_string
 
 
 @channel_session
 def ws_connect(message):
     channel = message.reply_channel
     channel.send({"accept": True})
+    channel.send(to_text_dict("Welcome to the Go Server. Use \"list_games\", "
+                              "\"create_game\" or \"join_game <game_id> <username> <color>\""))
 
 
 @channel_session
 def ws_message(message):
     command = message.content['text']
     channel = message.reply_channel
+
+    if command == 'list_games':
+        games_json_arr = []
+        for game_meta in GameMeta.objects.all():
+            games_json_arr.append(game_meta.serialize_to_json())
+        channel.send(to_text_dict(json.dumps(games_json_arr)))
+        return
+
+    if command.startswith('create_game'):  # support parameters: board_size, komi, time_settings, what else? TODO
+        game_meta = GameMeta.objects.create_game()
+        channel.send(to_text_dict("Created game with ID " + game_meta.game_id))
+        return
+
+    if command.startswith('join_game'):
+        params = command.split(' ')[1:]
+        if len(params) != 3:
+            channel.send(to_text_dict("join_game requires three arguments: \"join_game <game_id> <username> <color>\""))
+            return
+
+        game_id = params[0]
+        username = params[1]
+        color = normalize_color_string(params[2])
+
+        if not GameMeta.objects.filter(game_id=game_id).exists():
+            channel.send(to_text_dict("There is no game with the ID " + game_id))
+            return
+
+        game_meta = GameMeta.objects.get(game_id=game_id)
+
+        if game_meta.status != GameStatus.WAITING_FOR_PLAYERS:
+            channel.send(to_text_dict("This game is not waiting for players."))
+            return
+
+        if not game_meta.color_available(color):
+            channel.send(to_text_dict("This color is already taken."))
+            return
+
+        if not game_meta.username_available(username):
+            channel.send(to_text_dict("The other player has the same username."))
+            return
+
+        # TODO
+
+        return
 
 
 @channel_session
--- go_server_app/utils.py
+++ go_server_app/utils.py
@@ -6,3 +6,17 @@ def gen_random_string(n, lower_case=True):
     options = string.digits + (string.ascii_lowercase if lower_case else string.ascii_uppercase)
     # via stackoverflow.com/a/23728630/2474159
     return ''.join(random.SystemRandom().choice(options) for _ in range(n))
+
+
+# for sending text over channels, convenience method
+def to_text_dict(text):
+    return {"text": text}
+
+
+def normalize_color_string(col):
+    col = col.lower()
+    if col == 'black':
+        col = 'b'
+    if col == 'white':
+        col = 'w'
+    return col
