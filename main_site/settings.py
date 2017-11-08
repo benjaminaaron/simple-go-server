d8908f7536d4f84a3f824608e35d557d68cc85bf
Benjamin Aaron Degenhart <b.degenhart.study@gmail.com>
2017-11-08 16:56:27 +0100

    register go_server_app in settings

--- main_site/settings.py
+++ main_site/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'go_server_app.apps.GoServerAppConfig',
 ]
 
 MIDDLEWARE = [
