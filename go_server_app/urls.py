f51d172f6f4e39d40bdf36ad57b4825a6ec8008c
Benjamin Aaron Degenhart <b.degenhart.study@gmail.com>
2017-11-08 20:41:22 +0100

    add dashboard and game view

--- go_server_app/urls.py
+++ go_server_app/urls.py
@@ -5,4 +5,6 @@ from . import views
 app_name = 'go_server_app'  # namespacing
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^dashboard/$', views.dashboard, name='dashboard'),
+    url(r'^game/(?P<game_id>[a-zA-Z0-9]+)$', views.game, name='game'),
 ]
--- go_server_app/views.py
+++ go_server_app/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 
+from .models import GameMeta
+
 
 def index(request):
     return render(request, 'go_server_app/index.html')
+
+
+def dashboard(request):
+    return render(request, 'go_server_app/dashboard.html', {'games_list': GameMeta.objects.all()})
+
+
+def game(request, game_id):
+    game_meta = GameMeta.objects.get(game_id=game_id)
+    return render(request, 'go_server_app/game.html', {'game_meta': game_meta})
